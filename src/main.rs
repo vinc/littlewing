3f00ff8d83dafae6022ea20f1aa3a869c1fe151b
Vincent Ollivier <v@vinc.cc>
2014-12-23 17:49:40 +0100

    Read user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
+fn usage() {
+    println!("Usage:");
+    println!("quit    exit this program");
+}
+
 fn main() {
     println!("Little Wing v0.0.1");
+    println!("");
+
+    loop {
+        print!("> ");
+        let line = io::stdin().read_line().unwrap();
+        let cmd = line.as_slice().trim();
+        match cmd {
+            "quit" => break,
+            _      => usage()
+        }
+    }
 }
