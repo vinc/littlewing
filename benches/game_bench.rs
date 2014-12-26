467f1793604cb904a115b2eaab987c581e5841c8
Vincent Ollivier <v@vinc.cc>
2014-12-26 16:03:41 +0100

    Fix bench

--- benches/game_bench.rs
+++ benches/game_bench.rs
@@ -6,7 +6,7 @@ use littlewing::game::Game;
 
 #[bench]
 fn bench_game_perft(b: &mut Bencher) {
-    let game = Game::new();
+    let mut game = Game::new();
 
     b.iter(|| {
         game.perft(5);
