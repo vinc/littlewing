31582491555301df372fe9b365428e3055918158
Vincent Ollivier <v@vinc.cc>
2015-01-13 22:52:22 +0100

    Add pieces values to eval

--- src/littlewing/eval.rs
+++ src/littlewing/eval.rs
@@ -1,19 +1,59 @@
 use std::num::Int;
 
+use littlewing::common::*;
 use littlewing::game::Game;
 
+pub const PAWN_VALUE:   i32 = 100;
+pub const KNIGHT_VALUE: i32 = 325;
+pub const BISHOP_VALUE: i32 = 325;
+pub const ROOK_VALUE:   i32 = 500;
+pub const QUEEN_VALUE:  i32 = 965; // Rook + Minor + Pawn + Bishop Pair
+pub const KING_VALUE:   i32 = 10000;
+
 pub trait Eval {
-    fn eval(&mut self) -> i32;
+    fn eval_pieces(&self, c: Color) -> i32;
+    fn eval(&self) -> i32;
 }
 
 impl Eval for Game {
-    fn eval(&mut self) -> i32 {
-        let side = self.positions.top().side;
+    fn eval_pieces(&self, c: Color) -> i32 {
         let mut score = 0;
 
-        score += self.bitboards[side].count_ones() as i32;
-        score -= self.bitboards[side ^ 1].count_ones() as i32;
+        score += PAWN_VALUE * self.bitboards[c | PAWN].count_ones() as i32;
+        score += KNIGHT_VALUE * self.bitboards[c | KNIGHT].count_ones() as i32;
+        score += BISHOP_VALUE * self.bitboards[c | BISHOP].count_ones() as i32;
+        score += ROOK_VALUE * self.bitboards[c | ROOK].count_ones() as i32;
+        score += QUEEN_VALUE * self.bitboards[c | QUEEN].count_ones() as i32;
+        score += KING_VALUE * self.bitboards[c | KING].count_ones() as i32;
 
         score
+    }
+
+    fn eval(&self) -> i32 {
+        let mut score = 0;
+        let side = self.positions.top().side;
+
+        score += self.eval_pieces(side);
+        score -= self.eval_pieces(side ^ 1);
+
+        score
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate test;
+    use self::test::Bencher;
+    use littlewing::common::*;
+    use littlewing::game::Game;
+    use littlewing::eval::Eval;
+    use littlewing::fen::FEN;
+
+    #[bench]
+    fn bench_eval(b: &mut Bencher) {
+        let mut game: Game = FEN::from_fen(DEFAULT_FEN);
+        b.iter(|| {
+            game.eval()
+        })
     }
 }
