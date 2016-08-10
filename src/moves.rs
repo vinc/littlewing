bdf07a168624f668db1ff6c1d1d72c0aa4f79090
Vincent Ollivier <v@vinc.cc>
2016-08-10 00:00:25 +0000

    Implement Display for Move

--- src/moves.rs
+++ src/moves.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::ops::Index;
 
 use common::*;
@@ -13,48 +14,65 @@ impl Move {
     pub fn new(from: Square, to: Square, mt: MoveType) -> Move {
         Move(((from as u16) << 10) | ((to as u16) << 4) | mt as u16)
     }
+
     pub fn new_null() -> Move {
         Move(0)
     }
+
     pub fn from(&self) -> Square {
         let Move(v) = *self;
         (v >> 10) as Square
     }
+
     pub fn to(&self) -> Square {
         let Move(v) = *self;
         ((v >> 4) & 0b111111) as Square
     }
+
     pub fn kind(&self) -> MoveType {
         let Move(v) = *self;
         (v & 0b1111) as MoveType
     }
+
     pub fn is_null(&self) -> bool {
         let Move(v) = *self;
         v == 0
     }
+
     pub fn is_capture(&self) -> bool {
         self.kind() == CAPTURE || self.kind() & 0b1100 == 0b1100
     }
+
     pub fn is_castle(&self) -> bool {
         self.kind() == KING_CASTLE || self.kind() == QUEEN_CASTLE
     }
+
     pub fn castle_kind(&self) -> Piece {
         QUEEN_CASTLE << self.kind() - 1
     }
+
     pub fn is_promotion(&self) -> bool {
         self.kind() & PROMOTION_MASK > 0
     }
+
     pub fn promotion_kind(&self) -> Piece {
         PROMOTION_KINDS[(self.kind() & PROMOTION_KIND_MASK >> 2) as usize]
     }
+
     pub fn to_can(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl fmt::Display for Move {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut out = String::new();
         out.push_str(self.from().to_coord().as_str());
         out.push_str(self.to().to_coord().as_str());
         if self.is_promotion() {
             out.push((BLACK | self.promotion_kind()).to_char());        
         }
-        out
+        write!(f, "{}", out)
     }
 }
 
@@ -72,29 +90,38 @@ impl Moves {
             ply: 0
         }
     }
+
     pub fn inc(&mut self) {
         self.ply += 1;
     }
+
     pub fn dec(&mut self) {
         self.ply -= 1;
     }
+
     pub fn clear(&mut self) {
         self.lens[self.ply] = 0;
     }
+
     pub fn clear_all(&mut self) {
         self.lens = [0; MAX_PLY];
         self.ply = 0;
     }
+
     pub fn len(&self) -> usize {
         self.lens[self.ply]
     }
+
+    #[allow(dead_code)]
     pub fn is_empty(&self) -> bool {
         self.lens[self.ply] == 0
     }
+
     pub fn add_move(&mut self, from: Square, to: Square, mt: MoveType) {
         self.lists[self.ply][self.lens[self.ply]] = Move::new(from, to, mt);
         self.lens[self.ply] += 1;
     }
+
     pub fn add_moves(&mut self, mut targets: Bitboard, dir: Direction, mt: MoveType) {
         while targets != 0 {
             let to = targets.trailing_zeros() as Square;
@@ -105,6 +132,7 @@ impl Moves {
             targets.reset(to);
         }
     }
+
     pub fn add_moves_from(&mut self, mut targets: Bitboard, from: Square, mt: MoveType) {
         while targets != 0 {
             let to = targets.trailing_zeros() as Square;
@@ -112,6 +140,7 @@ impl Moves {
             targets.reset(to);
         }
     }
+
     pub fn add_pawns_moves(&mut self, bitboards: &[Bitboard], side: Color, ep: Square) {
         const XDIRS: [Direction; 2] = [LEFT, RIGHT];
         const YDIRS: [Direction; 2] = [UP, DOWN];
@@ -150,6 +179,7 @@ impl Moves {
             self.add_moves(attacks & END_RANKS[side as usize], dir, QUEEN_PROMOTION_CAPTURE);
         }
     }
+
     pub fn add_knights_moves(&mut self, bitboards: &[Bitboard], side: Color) {
         let occupied = bitboards[(WHITE) as usize] | bitboards[(BLACK) as usize];
         let mut knights = bitboards[(side | KNIGHT) as usize];
@@ -163,6 +193,7 @@ impl Moves {
             knights.reset(from);
         }
     }
+
     pub fn add_king_moves(&mut self, bitboards: &[Bitboard], side: Color) {
         let occupied = bitboards[WHITE as usize] | bitboards[BLACK as usize];
         let mut kings = bitboards[(side | KING) as usize];
@@ -176,6 +207,7 @@ impl Moves {
             kings.reset(from);
         }
     }
+
     pub fn add_bishops_moves(&mut self, bitboards: &[Bitboard], side: Color) {
         let occupied = bitboards[WHITE as usize] | bitboards[BLACK as usize];
         let mut bishops = bitboards[(side | BISHOP) as usize];
@@ -187,6 +219,7 @@ impl Moves {
             bishops.reset(from);
         }
     }
+
     pub fn add_rooks_moves(&mut self, bitboards: &[Bitboard], side: Color) {
         let occupied = bitboards[WHITE as usize] | bitboards[BLACK as usize];
         let mut rooks = bitboards[(side | ROOK) as usize];
@@ -198,6 +231,7 @@ impl Moves {
             rooks.reset(from);
         }
     }
+
     pub fn add_queens_moves(&mut self, bitboards: &[Bitboard], side: Color) {
         let occupied = bitboards[WHITE as usize] | bitboards[BLACK as usize];
         let mut queens = bitboards[(side | QUEEN) as usize];
@@ -210,9 +244,11 @@ impl Moves {
             queens.reset(from);
         }
     }
+
     pub fn add_king_castle(&mut self, side: Color) {
         self.add_move(E1 ^ 56 * side, G1 ^ 56 * side, KING_CASTLE);
     }
+
     pub fn add_queen_castle(&mut self, side: Color) {
         self.add_move(E1 ^ 56 * side, C1 ^ 56 * side, QUEEN_CASTLE);
     }
@@ -220,6 +256,7 @@ impl Moves {
 
 impl Index<usize> for Moves {
     type Output = Move;
+
     fn index(&self, _index: usize) -> &Move {
         &self.lists[self.ply][_index]
     }
@@ -236,15 +273,21 @@ mod tests {
         assert_eq!(Move::new(E1, G1, KING_CASTLE).is_castle(), true);
         assert_eq!(Move::new(E1, C1, QUEEN_CASTLE).is_castle(), true);
     }
+
+    #[test]
     fn test_move_castle_kind() {
         assert_eq!(Move::new(E1, G1, KING_CASTLE).castle_kind(), KING);
         assert_eq!(Move::new(E1, C1, QUEEN_CASTLE).castle_kind(), QUEEN);
     }
+
+    #[test]
     fn test_move_is_promotion() {
         assert_eq!(Move::new(E2, E3, QUIET_MOVE).is_promotion(), false);
         assert_eq!(Move::new(E7, E8, QUEEN_PROMOTION).is_promotion(), true);
         assert_eq!(Move::new(E7, D8, QUEEN_PROMOTION_CAPTURE).is_promotion(), true);
     }
+
+    #[test]
     fn test_move_promotion_kind() {
         assert_eq!(Move::new(E7, E8, QUEEN_PROMOTION).promotion_kind(), QUEEN);
         assert_eq!(Move::new(E7, D8, ROOK_PROMOTION_CAPTURE).promotion_kind(), ROOK);
