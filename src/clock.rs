a9a2803cdee4b6c35a7a93dadd2faa2dd65112f5
Vincent Ollivier <v@vinc.cc>
2016-08-17 10:25:34 +0000

    Allow more time to play a move in time management

--- src/clock.rs
+++ src/clock.rs
@@ -26,6 +26,7 @@ impl Clock {
             is_finished: false,
         }
     }
+
     pub fn start(&mut self, ply: usize) {
         self.is_finished = false;
         self.last_nodes_count = 0;
@@ -35,30 +36,34 @@ impl Clock {
         let moves_done = (((ply - 1) / 2) as u16) % self.moves_level;
         self.moves_remaining = self.moves_level - moves_done;
     }
+
     pub fn set_time(&mut self, time: u64) {
         self.time_remaining = time;
     }
-    pub fn allocated_time(&self) -> u64 {
-        let time_per_move = self.time_remaining / self.moves_remaining as u64;
-        let time_between_polls = self.polling_nodes_count / 4;
-        let time_to_play = 5;
-        let delta = time_between_polls + time_to_play;
 
-        if time_per_move < delta {
-            0
-        } else {
-            time_per_move - delta
-        }
+    pub fn allocated_time(&self) -> u64 {
+        self.time_remaining / self.moves_remaining as u64
     }
+
     pub fn elapsed_time(&self) -> u64 {
-        ((time::precise_time_s() * 1000.0) as u64) - self.started_at
+        let now = (time::precise_time_s() * 1000.0) as u64;
+
+        now - self.started_at
     }
+
     pub fn poll(&mut self, nodes_count: u64) -> bool {
         // We do the real computation only every `polling_nodes_count` nodes
         // TODO: do we need this?
         if nodes_count - self.last_nodes_count > self.polling_nodes_count {
             self.last_nodes_count = nodes_count;
-            self.is_finished = self.elapsed_time() > self.allocated_time();
+
+            // A certain amount of time pass between two polls,
+            // and after the end of the search.
+            let time_between_polls = self.polling_nodes_count / 4;
+            let time_to_play = 25;
+            let delta = time_between_polls + time_to_play;
+
+            self.is_finished = delta + self.elapsed_time() > self.allocated_time();
         }
 
         self.is_finished
