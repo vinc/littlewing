5794b907136d1c359d96b26ee7f6bb6ff05ced4a
Vincent Ollivier <v@vinc.cc>
2016-09-03 03:44:24 +0000

    Add bonus for bishup pair

--- src/eval.rs
+++ src/eval.rs
@@ -7,12 +7,14 @@ use bitboard::{BitboardExt, BitboardIterator};
 use game::Game;
 use moves::Move;
 
-pub const PAWN_VALUE:   Score = 100;
-pub const KNIGHT_VALUE: Score = 325;
-pub const BISHOP_VALUE: Score = 325;
-pub const ROOK_VALUE:   Score = 500;
-pub const QUEEN_VALUE:  Score = 965; // Rook + Minor + Pawn + Bishop Pair
-pub const KING_VALUE:   Score = 10000;
+pub const PAWN_VALUE:        Score = 100;
+pub const KNIGHT_VALUE:      Score = 350;
+pub const BISHOP_VALUE:      Score = 350;
+pub const ROOK_VALUE:        Score = 500;
+pub const QUEEN_VALUE:       Score = 1000; // R + B + P + bonus bishop pair
+pub const KING_VALUE:        Score = 10000;
+
+pub const BONUS_BISHOP_PAIR: Score = 50;
 
 lazy_static! {
     pub static ref PIECE_VALUES: [Score; 14] = {
@@ -46,10 +48,14 @@ impl Eval for Game {
     fn eval_material(&self, c: Color) -> Score {
         let mut score = 0;
 
-        for p in PIECES.iter() {
+        for &p in PIECES.iter() {
             let piece = c | p;
             let n = self.bitboards[piece as usize].count() as Score;
             score += n * PIECE_VALUES[piece as usize];
+
+            if p == BISHOP && n == 2 {
+                score += BONUS_BISHOP_PAIR;
+            }
         }
 
         score
@@ -151,11 +157,11 @@ mod tests {
     fn test_see() {
         let fen = "1k1r4/1pp4p/p7/4p3/8/P5P1/1PP4P/2K1R3 w - -";
         let game = Game::from_fen(fen);
-        assert_eq!(game.see(Move::new(E1, E5, CAPTURE)), 100);
+        assert_eq!(game.see(Move::new(E1, E5, CAPTURE)), PAWN_VALUE);
 
         let fen = "1k1r3q/1ppn3p/p4b2/4p3/8/P2N2P1/1PP1R1BP/2K1Q3 w - -";
         let game = Game::from_fen(fen);
-        assert_eq!(game.see(Move::new(D3, E5, CAPTURE)), -225);
+        assert_eq!(game.see(Move::new(D3, E5, CAPTURE)), PAWN_VALUE - KNIGHT_VALUE);
 
         let fen = "rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 2";
         let game = Game::from_fen(fen);
@@ -168,12 +174,12 @@ mod tests {
         let fen = "rnbqkb1r/pp2pppp/2p2n2/1B1p4/4P3/2N5/PPPP1PPP/R1BQK1NR w KQkq - 0 4";
         let game = Game::from_fen(fen);
         assert_eq!(game.see(Move::new(E4, D5, CAPTURE)), 0);
-        assert_eq!(game.see(Move::new(C3, D5, CAPTURE)), -225);
-        assert_eq!(game.see(Move::new(B5, C6, CAPTURE)), -225);
+        assert_eq!(game.see(Move::new(C3, D5, CAPTURE)), PAWN_VALUE - KNIGHT_VALUE);
+        assert_eq!(game.see(Move::new(B5, C6, CAPTURE)), PAWN_VALUE - BISHOP_VALUE);
 
         let fen = "rnbqkbnr/pppp1ppp/8/4p3/3P4/8/PPP1PPPP/RNBQKBNR w KQkq e6 0 2";
         let game = Game::from_fen(fen);
-        assert_eq!(game.see(Move::new(D4, E5, CAPTURE)), 100);
+        assert_eq!(game.see(Move::new(D4, E5, CAPTURE)), PAWN_VALUE);
     }
 }
 
