b99af33588e3c1d5b54d564c642958f4a3fa43eb
Vincent Ollivier <v@vinc.cc>
2018-07-02 08:05:31 +0200

    Refactor code in hyperbola init

--- src/hyperbola.rs
+++ src/hyperbola.rs
@@ -34,39 +34,11 @@ fn rank_attacks(occupied: Bitboard, sq: Square) -> Bitboard {
     let file = sq.file() as Bitboard;
     let rankx8 = (sq.rank() * 8) as Bitboard;
     let occupied = (occupied >> rankx8) & 2 * 63;
-    
+
     RANK_ATTACKS[(4 * occupied + file) as usize] << rankx8
 }
 
-fn is_out_rank(dir: Direction, sq: Square) -> bool {
-    let crossed_north = dir.is_north() && sq.rank() == 7;
-    let crossed_south = dir.is_south() && sq.rank() == 0;
-    crossed_north || crossed_south
-}
-
-fn is_out_file(dir: Direction, sq: Square) -> bool {
-    let crossed_west = dir.is_west() && sq.file() == 0;
-    let crossed_east = dir.is_east() && sq.file() == 7;
-    crossed_west || crossed_east
-}
-
-fn genmask(dir: Direction, sq: Square) -> Bitboard {
-    debug_assert!(sq < OUT);
-    let shift = DIRECTION_SHIFTS[dir];
-    let mut bb = 0;
-    let mut dest = ((sq as Shift) + shift) as Square;
-    loop {
-        if is_out_file(dir, dest) || dest >= OUT {
-            break;
-        }
-        bb |= Bitboard::from_square(dest);
-        if is_out_rank(dir, dest) {
-            break;
-        }
-        dest = ((dest as Shift) + shift) as Square;
-    }
-    bb
-}
+// Initialization of HYPERBOLA_MASKS and RANK_ATTACKS
 
 lazy_static! {
     static ref HYPERBOLA_MASKS: [[Bitboard; 4]; 64] = {
@@ -100,8 +72,40 @@ lazy_static! {
                         break;
                     }
                 }
+                println!("occ={} file={} i={}", occ, file, i);
+                rank_attacks[i].debug()
             }
         }
         rank_attacks
     };
+}
+
+fn is_out_rank(dir: Direction, sq: Square) -> bool {
+    let crossed_north = dir.is_north() && sq.rank() == 7;
+    let crossed_south = dir.is_south() && sq.rank() == 0;
+    crossed_north || crossed_south
+}
+
+fn is_out_file(dir: Direction, sq: Square) -> bool {
+    let crossed_west = dir.is_west() && sq.file() == 0;
+    let crossed_east = dir.is_east() && sq.file() == 7;
+    crossed_west || crossed_east
+}
+
+fn genmask(dir: Direction, sq: Square) -> Bitboard {
+    debug_assert!(sq < OUT);
+    let shift = DIRECTION_SHIFTS[dir];
+    let mut bb = 0;
+    let mut dest = ((sq as Shift) + shift) as Square;
+    loop {
+        if is_out_file(dir, dest) || dest >= OUT {
+            break;
+        }
+        bb |= Bitboard::from_square(dest);
+        if is_out_rank(dir, dest) {
+            break;
+        }
+        dest = ((dest as Shift) + shift) as Square;
+    }
+    bb
 }
