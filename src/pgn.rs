6435f3abcf36c88c04c7a1f2bb8f59454097dda3
Vincent Ollivier <v@vinc.cc>
2019-11-27 07:26:11 +0100

    implement From<String> for PGN

--- src/pgn.rs
+++ src/pgn.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 use std::collections::BTreeMap;
 use std::fmt;
 
@@ -17,18 +19,34 @@ pub struct PGN {
     game: String,
 }
 
+lazy_static! {
+    static ref DEFAULT_HEADERS: Vec<(String, String)> = vec![
+        ("1Event".to_string(), "?".to_string()),
+        ("2Site".to_string(), "?".to_string()),
+        ("3White".to_string(), "?".to_string()),
+        ("4Black".to_string(), "?".to_string()),
+        ("5Result".to_string(), "*".to_string()),
+    ];
+}
+
 impl PGN {
     fn new() -> PGN {
         PGN {
-            headers: vec![
-                ("1Event".to_string(), "?".to_string()),
-                ("2Site".to_string(), "?".to_string()),
-                ("3White".to_string(), "?".to_string()),
-                ("4Black".to_string(), "?".to_string()),
-                ("5Result".to_string(), "?".to_string())
-            ].into_iter().collect(),
+            headers: DEFAULT_HEADERS.clone().into_iter().collect(),
             game: "".to_string(),
         }
+    }
+
+    pub fn white(&self) -> String {
+        self.headers["3White"].clone()
+    }
+
+    pub fn black(&self) -> String {
+        self.headers["4Black"].clone()
+    }
+
+    pub fn result(&self) -> String {
+        self.headers["5Result"].clone()
     }
 
     pub fn set_white(&mut self, white: &str) {
@@ -44,8 +62,12 @@ impl PGN {
     }
 
     pub fn set_fen(&mut self, fen: &str) {
-        self.headers.insert("6FEN".to_string(), fen.to_string());
-        self.headers.insert("7SetUp".to_string(), "1".to_string());
+        self.headers.insert("FEN".to_string(), fen.to_string());
+        self.headers.insert("SetUp".to_string(), "1".to_string());
+    }
+
+    pub fn set_header(&mut self, key: &str, val: &str) {
+        self.headers.insert(key.to_string(), val.to_string());
     }
 }
 
@@ -55,8 +77,40 @@ impl fmt::Display for PGN {
             writeln!(f, "[{} \"{}\"]", key.trim_start_matches(char::is_numeric), val)?;
         }
         writeln!(f, "")?;
-        writeln!(f, "{}{}", self.game, self.headers["5Result"])?;
+        writeln!(f, "{}", self.game)?;
         Ok(())
+    }
+}
+
+impl From<String> for PGN {
+    fn from(s: String) -> PGN {
+        lazy_static! {
+            static ref RE: Regex = Regex::new("\\[(?P<key>\\w+) \"(?P<val>.*)\"\\]").unwrap();
+        }
+        let mut pgn = PGN::new();
+        for line in s.lines() {
+            match RE.captures(line) {
+                Some(header) => {
+                    let key = header["key"].to_string();
+                    let val = header["val"].to_string();
+                    let mut is_default_header = false;
+                    for (k, _) in DEFAULT_HEADERS.iter() {
+                        if key == k.trim_start_matches(char::is_numeric) {
+                            pgn.set_header(k, &val);
+                            is_default_header = true;
+                            break;
+                        }
+                    }
+                    if !is_default_header {
+                        pgn.set_header(&key, &val);
+                    }
+                }
+                None => {
+                    pgn.game.push_str(line)
+                }
+            }
+        }
+        pgn
     }
 }
 
@@ -71,22 +125,22 @@ impl ToPGN for Game {
         let mut pgn = PGN::new();
 
         let starting_fen = self.starting_fen.clone();
-
         if starting_fen != DEFAULT_FEN {
             pgn.set_fen(&starting_fen);
         }
 
-        if self.is_mate() {
+        let result = if self.is_mate() {
             if self.is_check(WHITE) {
-                pgn.set_result("0-1");
+                "0-1"
             } else if self.is_check(BLACK) {
-                pgn.set_result("1-0");
+                "1-0"
             } else {
-                pgn.set_result("1/2-1/2");
+                "1/2-1/2"
             }
         } else {
-            pgn.set_result("*");
-        }
+            "*"
+        };
+        pgn.set_result(result);
 
         let moves = self.history.clone();
         self.load_fen(&starting_fen);
@@ -120,7 +174,7 @@ impl ToPGN for Game {
                 line.push(' ');
             }
         }
-        pgn.game.push_str(&format!("{}", line));
+        pgn.game.push_str(&format!("{}{}", line, result));
 
         pgn
     }
@@ -157,9 +211,21 @@ mod tests {
 
         let path = Path::new("tests/fool.pgn");
         let mut file = File::open(&path).unwrap();
-        let mut res = String::new();
-        file.read_to_string(&mut res).unwrap();
+        let mut content = String::new();
+        file.read_to_string(&mut content).unwrap();
 
-        assert_eq!(pgn.to_string(), res);
+        assert_eq!(pgn.to_string(), content);
+    }
+
+    #[test]
+    fn test_string_to_pgn() {
+        let path = Path::new("tests/fool.pgn");
+        let mut file = File::open(&path).unwrap();
+        let mut content = String::new();
+        file.read_to_string(&mut content).unwrap();
+
+        let pgn = PGN::from(content.clone());
+        assert_eq!(pgn.to_string(), content);
+        assert_eq!(pgn.result(), "0-1".to_string());
     }
 }
