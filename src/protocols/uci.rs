99ca12379d139785f009b7db4fa6f288a1f855f7
Vincent Ollivier <v@vinc.cc>
2024-10-08 12:42:08 +0200

    Run cargo clippy

--- src/protocols/uci.rs
+++ src/protocols/uci.rs
@@ -73,7 +73,7 @@ impl UCI {
         self.abort_search();
 
         let side = self.game.side();
-        let mut time = u64::max_value(); // Infinite time
+        let mut time = u64::MAX; // Infinite time
         let mut moves = 0;
         let mut next_arg_is_time = false;
         let mut next_arg_is_moves = false;
