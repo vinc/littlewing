20c136463067334a9e7cc9897baf819be9c5321f
Vincent Ollivier <v@vinc.cc>
2026-02-03 07:54:45 +0100

    Use score constants in sort

--- src/piece_move_generator.rs
+++ src/piece_move_generator.rs
@@ -117,11 +117,11 @@ impl PieceMoveGenerator for Game {
                     self.moves[i].score += GOOD_CAPTURE_SCORE;
                 }
                 debug_assert!(self.moves[i].score < BEST_MOVE_SCORE);
-                debug_assert!(self.moves[i].score > 0);
-            } else {
-                let m = self.moves[i].item;
-                self.moves[i].score = self.get_history(m) - HH_MAX;
-                debug_assert!(self.moves[i].score < 0);
+                debug_assert!(self.moves[i].score > QUIET_MOVE_SCORE);
+            } else if self.moves[i].score == QUIET_MOVE_SCORE {
+                let history_score = self.get_history(self.moves[i].item);
+                self.moves[i].score = history_score - HH_MAX;
+                debug_assert!(self.moves[i].score < QUIET_MOVE_SCORE);
                 debug_assert!(self.moves[i].score >= -HH_MAX);
             }
             for j in a..i {
